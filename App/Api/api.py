1a789febeb0d9331c6c2fde160b7372e2ef18a5a
leoniegtt <leonie.goutte@gmail.com>
2023-12-05 14:08:40 +0100

    update mieux mais pb index/label

--- App/Api/api.py
+++ App/Api/api.py
@@ -16,19 +16,22 @@ def get_questions():
 
     #compute positive rate for each feature
     rate = []
-    for i in range (1,nb_col): #we don't start at 0 since 0 is id and not a label
+    for i in range (0,nb_col): #we don't start at 0 since 0 is id and not a label
         count = 0
-        for j in range (0, nb_rows):
+        for j in range (1, nb_rows):
             count = count + grid[i][j]
         print("count=", count)
         rate.append(count/nb_rows) #to do check that =/ 0
     
     #choose feature (the one with positive rate closest to 0.5)
     closest_rate = min(rate, key=lambda x: abs(x - 0.5))
-    closest_index = rate.index(closest_rate)
+    closest_index = rate.index(0.5)
+    chosen_label=label[closest_index]
+
+    print("chosen label", chosen_label)
 
     print("Closest rate to 0.5:", closest_rate)
-    print("corresponding label:", label[closest_index])
+    print("corresponding label:", chosen_label)
 
     #ask corresponding question
 
