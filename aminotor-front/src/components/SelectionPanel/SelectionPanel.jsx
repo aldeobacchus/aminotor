c0203843b31dbc3288992e16f0975a3be116a002
iSpeX1 <ispexgaming@gmail.com>
2023-12-12 14:21:36 +0100

    selection image

--- aminotor-front/src/components/SelectionPanel/SelectionPanel.jsx
+++ aminotor-front/src/components/SelectionPanel/SelectionPanel.jsx
@@ -1,16 +1,8 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './SelectionPanel.css';
 
-const SelectionPanel = ({ size, onImageSelect }) => {
-  const [selectedImage, setSelectedImage] = React.useState(null);
-
-  // generate a list of 1024  random numbers between 000001 and 000200
-  const squares = [];
-  for (let i = 0; i < 1024; i++) {
-    const random = Math.floor(Math.random() * 200) + 1;
-    const padded = random.toString().padStart(6, '0');
-    squares.push(padded);
-  }
+const SelectionPanel = ({ size, squares, onImageSelect}) => {
+  const [selectedImage, setSelectedImage] = useState(null);
 
   // display only the first 'size' elements of the 'squares' list
   const squaresToDisplay = squares.slice(0, 2**(size*2));
--- aminotor-front/src/containers/Test/Test.jsx
+++ aminotor-front/src/containers/Test/Test.jsx
@@ -5,16 +5,27 @@ import SelectionPanel from '../../components/SelectionPanel/SelectionPanel';
 function Test() {
   const [sliderValue, setSliderValue] = useState(2);
   const [selectedImage, setSelectedImage] = useState(null);
+  const [squares, setSquares] = useState([]);
 
   const handleSliderChange = (value) => {
     setSliderValue(value);
   };
 
+  React.useEffect(() => {
+    const newSquares = [];
+    for (let i = 0; i < 1024; i++) {
+      const random = Math.floor(Math.random() * 200) + 1;
+      const padded = random.toString().padStart(6, '0');
+      newSquares.push(padded);
+    }
+    setSquares(newSquares);
+  }, []);
+
   return (
     <div>
       <SizePanelBar onSliderChange={handleSliderChange} />
-      <SelectionPanel size={sliderValue}  onImageSelect={setSelectedImage}/>
-      {selectedImage && <p>Selected image: {selectedImage}</p>}
+      <SelectionPanel size={sliderValue} squares={squares} onImageSelect={setSelectedImage}/>
+      {selectedImage && <img src={`/img/200/${selectedImage}.jpg`} alt="Selected" />}
     </div>
   );
 }
