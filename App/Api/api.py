ab0c5abeac49bced11262fb0ffa270f4ccc281d1
leoniegtt <leonie.goutte@gmail.com>
2024-01-10 11:45:28 +0100

    corrections logique

--- App/Api/api.py
+++ App/Api/api.py
@@ -8,26 +8,35 @@ from features import new_features, new_questions, new_answers  # Import new feat
 
 app = Flask(__name__)
 
+#initialisation du jeu : sélection de 1024 images
 @app.route('/api/init', methods=['GET'])
 def init_game():
-    #sélection de 1024 images au hasard
+    #sélection de 1024 id au hasard
     global list_image
     nb_images_bdd = 10000
     for i in range(1024):
         list_image.add(randrange(nb_images_bdd))
 
     #get images de la bdd à partir de leurs id
-        
     #renvoie les images au front
+        
 
-
+#première question du jeu
 @app.route('/api/start/{nb_images}', methods=['GET'])
 def start_game(nb_images):
     data = request.get_json()
 
-    global final_img_list 
-    global images, model, predicted_labels, nb_questions, max_questions, last_feature, proba_list
-    
+    #déclaration et initialisation des variables globales
+    global predicted_labels, nb_questions, max_questions, last_feature, proba_list, list_features
+    global final_img_list
+
+    final_img_list = []
+    list_features = new_features
+    nb_questions = 0
+    max_questions = 10
+    last_feature = None
+    proba_list = [0 for i in range(nb_images)]
+
     #create img list from size selected by user
     for i in range(nb_images):
         final_img_list.add(list_image[i])
@@ -35,94 +44,80 @@ def start_game(nb_images):
     #predict labels on selected images
     predicted_labels = load_process_predict(nb_images)
 
-    nb_questions = 0
-    max_questions = 10
-
-    last_feature = None
-
-    proba_list = [0 for i in range(nb_images)]
-
     #donner la première question
-    feature = get_questions(new_features, predicted_labels)
+    feature = get_questions(list_features, predicted_labels)
     question = new_questions[feature]
 
     return jsonify(
         feature=feature,
-        #param=param,
         question=question,
         answers=new_answers[feature],
-        #characterMatch=characterMatch
     )
 
 
-
-
+#update pour chaque question que l'on pose
 @app.route('/api/questions/', methods=['GET'])
-def get_questions():
-    already_features = request.json.get('alreadyFeatures', [])
-    params = request.json.get('params', [])
+def get_response_and_next_question():
+    #récupération de la réponse
     answers = request.json.get('answers', [])
 
-    # Use load_process_predict to get predictions based on the provided features
-    #characterMatch = load_process_predict(_nb_images=len(already_features))
+    #actualisation des probas
+    receive_answer(answers)
 
-    availableFeatures = set(new_features) - set(already_features)
-
-    #do the same for the labels
-
-    if not availableFeatures:
-        return jsonify(characterMatch=characterMatch)  # Return the character if all features have been processed
-
-    # Select a random new feature to ask a question about
-    #feature = random.choice(list(availableFeatures))
-    feature = get_questions(availableFeatures, )
-    question = new_questions[feature]
-    param = feature
-
-    # Prepare the response
-    if feature in already_features:
-        question += f" {characterMatch[feature]}?"
-        param = f"is_{characterMatch[feature]}"
-
-    nb_questions +=1
-    last_feature = param
-
-    return jsonify(
-        feature=feature,
-        param=param,
-        question=question,
-        answers=new_answers[feature],
-        characterMatch=characterMatch
-    )
-
-
-@app.route('/api/answer/', methods=['POST'])
-def receive_answer():
-    data = request.get_json()
-    user_answer = data.get('answer')
+    # Select a new feature to ask a question about
     if nb_questions < max_questions:
+        feature = get_questions(list_features, predicted_labels)
 
-        # Validation de la réponse de l'utilisateur
-        valid_answers = ["Yes", "No", "I don't know"]
-        if user_answer in valid_answers:
+        # Prepare the response
+        question += f" {new_questions[feature]}?"
+        param = f"is_{feature}"
+
+        nb_questions +=1
+        last_feature = feature
+
+        list_features[list_features.index(feature)] = None
+
+        return jsonify(
+            feature=feature,
+            param=param,
+            question=question,
+            answers=new_answers[feature],
+        ) 
             
-            #update probabilities from players' answer
-            index = new_features.index(last_feature)
-            for i in range (0, N-1):
-                proba_list[i] = proba_list[i] + predicted_labels[i][index]
-
-            response = {
-                "message": f"Received answer: {user_answer}"
-            }
-            return jsonify(response)
-        else:
-            return jsonify({"error": "Invalid answer. Use 'Yes', 'No', or 'I don't know'."}), 400
-        
     else : #max amount of questions reached => try to guess !!
 
         guess_index = proba_list.index(max(proba_list))
-        guess = new_features[guess_index]
+        guess = final_img_list[guess_index]
         # return quoi ???
+        return jsonify(
+            characterMatch=guess
+        ) 
+
+def receive_answer(user_answer):
+    N= len(proba_list)
+
+    # Validation de la réponse de l'utilisateur
+    valid_answers = ["Yes", "No", "I don't know"]
+    if user_answer in valid_answers:
+        if user_answer == "Yes":
+            answer = 1
+        elif user_answer == "No":
+            answer = 0
+        else:
+            answer = 0.5
+        
+        #update probabilities from players' answer
+        index = list_features.index(last_feature)
+        for i in range (0, N-1):
+            proba_list[i] = proba_list[i] + predicted_labels[i][index]*answer
+
+        response = {
+            "message": f"Received answer: {user_answer}"
+        }
+        return jsonify(response)
+    else:
+        return jsonify({"error": "Invalid answer. Use 'Yes', 'No', or 'I don't know'."}), 400
+
 
 if __name__ == '__main__':
     app.run(debug=True)
--- App/Api/questions.py
+++ App/Api/questions.py
@@ -1,29 +1,27 @@
-from features import new_features, new_questions, new_answers  # Import new features, questions, and answers
-
 def get_questions(labels, grid):
-
-    #get grid as an argument ? check the type of data and adapt code to match
-    #label= ["id", "brown_hair", "blue_eyes", "browneyes"]
-
-    #grid = [[5, 1,0,1],[6, 0,0,1],[7, 0,1,0], [8, 1,0,1],[9,0,0,0]]
-    #initialize grid with pics : id and then value for labels
 
     nb_rows = len(grid) #number of elements in the grid
     print("nb rows", nb_rows)
     nb_col = len(grid[0]) #number of features
     print("nb col", nb_col)
 
+    #init rate
+    rate = []
+    for i in (0, nb_col):
+        if labels[i]== None:
+            rate.append(None)
+        else :
+            rate.append(0)
 
     #compute positive rate for each feature
-    rate = [0]
-    for i in range (1,nb_col): #we don't start at 0 since 0 is id and not a label
-        count = 0
-        for j in range (0, nb_rows):
-            count = count + grid[j][i]
-        print("count=", count)
-        rate.append(count/nb_rows) #to do check that =/ 0
-    
-    #choose feature (the one with positive rate closest to 0.5)
+    for i in range (0,nb_col):
+        if rate[i]!= None:
+            count = 0
+            for j in range (0, nb_rows):
+                count = count + grid[j][i]
+            print("count=", count)
+            rate[i]=(count/nb_rows)
+
     closest_rate = min(rate, key=lambda x: abs(x - 0.5))
     closest_index = rate.index(closest_rate)
     chosen_label=labels[closest_index]
