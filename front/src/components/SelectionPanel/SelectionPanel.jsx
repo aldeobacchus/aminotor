cc1ce5ae53b333e78df02d6678c9f76af8399075
aldeobacchus <75472779+aldeobacchus@users.noreply.github.com>
2024-01-15 13:13:30 +0100

    css

--- front/src/components/SelectionPanel/SelectionPanel.jsx
+++ front/src/components/SelectionPanel/SelectionPanel.jsx
@@ -16,11 +16,12 @@ const SelectionPanel = ({ size, squares, onImageSelect}) => {
     <div key={size} className="selection-panel">
       {squaresToDisplay.map((square) => (
         <img 
+          className='square-img'
           key={square} 
           src={`https://etud.insa-toulouse.fr/~alami-mejjat/${square}.jpg`}
           alt={`${square}`}
           onClick={() => handleImageSelect(square)}
-          style={square === selectedImage ? { boxShadow: '0 0 10px 5px blue' } : {}}
+          style={square === selectedImage ? { boxShadow: '0 0 3px 5px #EDA828' } : {}}
         />
       ))}
     </div>
