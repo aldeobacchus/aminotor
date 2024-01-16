0301745bf33a20365d84785588aa683e1ae16515
KatonCat <raynoxd@gmail.com>
2024-01-16 12:54:53 +0100

    first commit

--- api/aminoguess.py
+++ api/aminoguess.py
@@ -6,66 +6,43 @@ from ml import load_process_predict, load_process_images  # Import your ML funct
 from flask_cors import cross_origin  # Fix the typo in import
 from features import new_features, new_questions, proba_features  # Import new features, questions, and answers
 from flask_cors import CORS
-from flask_session import Session
-import json
 import os
-import requests
 
 
 app = Flask(__name__)
-#app.secret_key = os.environ.get('SECRET_KEY') #KEEP THIS LINE AND ADD THE KEY ON AZURE
-app.secret_key = 'you-will-never-guess' # DON'T FORGET TO DELETE THIS LINE ON DEPLOYMENT
-app.config['SESSION_TYPE'] = 'filesystem'
-app.config['SESSION_PERMANENT'] = False
-app.config['SESSION_COOKIE_SAMESITE'] = 'None'
-app.config['SESSION_COOKIE_SECURE'] = True
-Session(app)
 CORS(app)
 
-#initialisation du jeu : sélection de 1024 images
-@app.route('/api/init/<int:gamemod>', methods=['GET'])
-@cross_origin(supports_credentials=True, origins="http://localhost:3000" )
-def init_game(gamemod):
-    response = requests.get(f'http://localhost:5001/image/init/{gamemod}').json()
-    session['list_image'] = response.get('list_image')
-    return jsonify(session['list_image'])
-
-
 #première question du jeu
-@app.route('/api/start/<int:nb_images>', methods=['GET'])
-@cross_origin(supports_credentials=True, origins="http://localhost:3000")
-def start_game(nb_images):
-
-    session['list_features'] = new_features.copy()
-    session['nb_images'] = nb_images
-    data={
-        'list_features': session['list_features'],
-        'nb_images': session['nb_images'],
-        'list_image': session['list_image']
-    }
-    
-    response = requests.post('http://localhost:5002/aminoguess/start', json=data).json()
-
-    #initialisation et update the session variables
-    session['max_questions'] = 10
-    session['last_feature'] = None
-    session['proba_list'] = [1]*nb_images
-    session['final_img_list'] = response.get("final_img_list")
-    session['last_feature'] = response.get("feature")
-    session['question'] = response.get("question")
-    session['nb_questions'] = 1
-    session['predicted_labels'] = response.get("predicted_labels")
-
+@app.route('/aminoguess/start', methods=['POST'])
+def start_game():
+    data = request.json
+    final_img_list = []
+    nb_image = data['nb_images']
+    list_image = data['list_image']
+    list_features = data['list_features']
+    #create img list from size selected by user
+    for i in range(nb_image):
+        final_img_list.append(list_image[i])
+    # create a list of path from the list of images
+    list_path = []  
+    for i in range(nb_image):
+        list_path.append("https://etud.insa-toulouse.fr/~alami-mejjat/0"+str(final_img_list[i])+".jpg")
+    #predict labels on selected images
+    predicted_labels = load_process_predict(list_path)
+    #donner la première question
+    feature = get_questions(list_features, predicted_labels)
+    question = new_questions[feature]
     return jsonify(
-        feature=response.get("feature"),
-        question=response.get("question")
+        final_img_list=final_img_list,
+        predicted_labels=predicted_labels.tolist(),
+        feature=feature,
+        question=question
     )
 
 
 #update pour chaque question que l'on pose
-@app.route('/api/answer/<int:answer>', methods=['GET'])
-@cross_origin(supports_credentials=True, origins="http://localhost:3000")
-def get_response_and_next_question(answer):
+#@app.route('/aminoguess/answer/<int:answer>', methods=['GET'])
+#def get_response_and_next_question(answer):
     #actualisation des probas
     if answer != 2:
         update_probabilities(answer)
@@ -110,9 +87,8 @@ def get_response_and_next_question(answer):
             question=question,
         ) 
     
-@app.route('/api/proposition/', methods=['GET'])
-@cross_origin(supports_credentials=True, origins="http://localhost:3000")
-def continue_next_question():
+#@app.route('/aminoguess/proposition/', methods=['GET'])
+#def continue_next_question():
     proba_list = session['proba_list']
     final_img_list = session['final_img_list']
     list_features = session['list_features']
@@ -139,6 +115,7 @@ def continue_next_question():
         question=question,
     ) 
 
+
 # Update the probabilities based on the user's answer
 def update_probabilities(user_answer):
     list_features = session['list_features']
@@ -161,4 +138,4 @@ def update_probabilities(user_answer):
     
 
 if __name__ == '__main__':
-    app.run(debug=True, port = 5000)
+    app.run(debug=True, port = 5002)
