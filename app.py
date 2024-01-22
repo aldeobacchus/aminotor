c5e2a980ca053137c0c548496b14869a78924e86
Gabriel <75472779+aldeobacchus@users.noreply.github.com>
2024-01-22 17:33:35 +0100

    Update app.py

--- app.py
+++ app.py
@@ -7,6 +7,9 @@ from flask_cors import CORS
 app = Flask(__name__)
 CORS(app)
 
+@app.route('/')
+def hello():
+    print("hello x")
 
 #MS du mode de jeu 2 : start
 #PENSEZ à RECUP la nouvelle version de FEATURES.PY
