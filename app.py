155dfa06febd5ffd61506679889ef4ff29f54a5a
Gabriel <75472779+aldeobacchus@users.noreply.github.com>
2024-01-23 14:30:59 +0100

    Update app.py

--- app.py
+++ app.py
@@ -9,7 +9,7 @@ import requests
 
 app = Flask(__name__)
 #app.secret_key = os.environ.get('SECRET_KEY') #KEEP THIS LINE AND ADD THE KEY ON AZURE
-app.secret_key = 'you-will-never-guess' # DON'T FORGET TO DELETE THIS LINE ON DEPLOYMENT
+#app.secret_key = 'you-will-never-guess' # DON'T FORGET TO DELETE THIS LINE ON DEPLOYMENT
 app.config['SESSION_TYPE'] = 'filesystem'
 app.config['SESSION_PERMANENT'] = False
 app.config['SESSION_COOKIE_SAMESITE'] = 'Lax' # change to 'None' in prod and 'Lax' with postman
