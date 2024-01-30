18ffc3a4bda3e956874b014a65dd0151fd61fb61
iSpeX1 <ispexgaming@gmail.com>
2024-01-30 17:36:41 +0100

    final ai

--- app.py
+++ app.py
@@ -28,22 +28,6 @@ images_dir_path = os.path.join(os.getcwd(), "images")
 nb_images = 10
 image_width, image_height = 178, 218
 
-
-def load_process_images(list_image_path):
-    # Load and preprocess the images
-    images = []
-    for image_path in list_image_path:
-        # Load and resize the image
-        img = tf.keras.utils.load_img(image_path, target_size=(image_height, image_width))
-        img_array = img_to_array(img)
-        # Preprocess the image (you may need to adapt this based on your specific requirements)
-        img_array = img_array / 255.0  # Normalize pixel values to [0, 1]
-
-        # Append the image and label to the lists
-        images.append(img_array)
-
-    return np.array(images)
-
 def load_images (list_images):
 
     images = []
@@ -78,26 +62,12 @@ def load_process_predict(_model_path=model_path):
 
     predicted_labels = []
 
-    list_path_upload = data['list_path_upload']
-
-    # TODO : change once we know how to get from the database
-    images = load_process_images(list_path_upload)
-
     list_path_init = data['list_path_init']
-    
-
-    # Load and preprocess the images
-    if len(list_path_init) > 0 :
-        if images.size == 0:
-            images = load_images(list_path_init)
-        else:
-            images = np.concatenate((images, load_images(list_path_init)), axis=0)
+    images = load_images(list_path_init)
 
     predicted_labels += np.round(model.predict(images)).tolist()
 
-
     return jsonify(predicted_labels=predicted_labels)
-
 
 if __name__ == '__main__':
     app.run(debug=True, port = 5003)
