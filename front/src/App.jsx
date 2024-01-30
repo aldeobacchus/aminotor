6ef998e05fc8702962e7dc4e368210169b41c81c
aldeobacchus <75472779+aldeobacchus@users.noreply.github.com>
2024-01-30 16:19:08 +0100

    finitions ui + loader +  more responsive

--- front/src/App.jsx
+++ front/src/App.jsx
@@ -4,11 +4,11 @@ import { useState } from 'react';
 import Game from './containers/Game/Game';
 
 function App() {
-  
+
   const [mode, setMode] = useState("home");
 
   //Callback to select the game
-  const onChangeGameMode = (varMode) =>{
+  const onChangeGameMode = (varMode) => {
     console.log("Game mode set to : " + varMode);
     setMode(varMode);
   }
@@ -19,7 +19,7 @@ function App() {
 
       {mode === 'home' && (
         <>
-          <h1 onClick={() => {setMode("home")}}>Aminotor</h1>
+          <h1 onClick={() => { setMode("home") }}>Aminotor</h1>
 
           <GameSelection callback={(e) => onChangeGameMode(e)} />
         </>
@@ -27,27 +27,27 @@ function App() {
 
       {mode === "Amino'Guess" && (
         <>
-          <h3 onClick={() => {setMode("home")}}>Aminotor</h3>
-          <Game gm="Amino'Guess" setMode={setMode}/>
+          <h3 onClick={() => { setMode("home") }}>Aminotor</h3>
+          <Game gm="Amino'Guess" setMode={setMode} />
         </>
       )}
 
       {mode === 'Ariane' && (
         <>
-          <h3 onClick={() => {setMode("home")}}>Ariane</h3>
-          <Game gm="Ariane" setMode={setMode}/>
+          <h3 onClick={() => { setMode("home") }}>Aminotor</h3>
+          <Game gm="Ariane" setMode={setMode} />
         </>
       )}
 
       {mode === 'Theseus Battle' && (
         <>
-          <h3 onClick={() => {setMode("home")}}>Theseus Battle</h3>
-          <Game gm="Theseus Battle" setMode={setMode}/>
+          <h3 onClick={() => { setMode("home") }}>Aminotor</h3>
+          <Game gm="Theseus Battle" setMode={setMode} />
         </>
-      )  
+      )
       }
 
-      
+
     </div>
   );
 }
